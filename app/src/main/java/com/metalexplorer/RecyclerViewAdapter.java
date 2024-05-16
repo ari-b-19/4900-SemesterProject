59d776a9d63ac30beb88fa997c60e82be5ccb321
Arian <arianborova@gmail.com>
2024-05-15 22:41:47 -0400

    bookmarks

--- app/src/main/java/com/metalexplorer/RecyclerViewAdapter.java
+++ app/src/main/java/com/metalexplorer/RecyclerViewAdapter.java
@@ -16,15 +16,25 @@ import java.util.Optional;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
 
     private final RecyclerViewInterface recyclerViewInterface;
-    private List<SearchBandResult> bandData; //
+    private List<SearchBandResult> bandData;
+    private List<Band> bandData2;//
 
     private RecyclerView recyclerView;
+
+    private boolean flag = false;
 
     // Constructor to initialize the data and inflater
     public RecyclerViewAdapter(ArrayList<SearchBandResult> data, RecyclerViewInterface recyclerViewInterface, RecyclerView recyclerView) {
         this.bandData = data;
         this.recyclerViewInterface = recyclerViewInterface;
         this.recyclerView = recyclerView;
+    }
+
+    public RecyclerViewAdapter(ArrayList<Band> data2, RecyclerViewInterface recyclerViewInterface, RecyclerView recyclerView, boolean flag) {
+        this.bandData2 = data2;
+        this.recyclerViewInterface = recyclerViewInterface;
+        this.recyclerView = recyclerView;
+        this.flag = flag;
     }
 
     @NonNull
@@ -36,24 +46,29 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Optional<String> optionalGenre = bandData.get(position).getGenre();
-        String empty = "No matches found";
+        if (flag != true) {
+            Optional<String> optionalGenre = bandData.get(position).getGenre();
+            String empty = "No matches found";
 
 //        Band band;
 //        for (int i = 0; i < band.getCurrentMembers(); i++) {
 //            band.getCurrentMembers().get(i).
 //        }
 
-        if (optionalGenre.isPresent()) {
-            String genre = optionalGenre.get();
-            holder.bandGenre.setText(genre);
+            if (optionalGenre.isPresent()) {
+                String genre = optionalGenre.get();
+                holder.bandGenre.setText(genre);
+            } else {
+                holder.bandGenre.setText("");
+            }
+            if (bandData.get(position) == null) {
+                holder.band.setText(empty);
+            } else {
+                holder.band.setText(bandData.get(position).getName());
+            }
         } else {
-            holder.bandGenre.setText("");
-        }
-        if (bandData.get(position) == null) {
-            holder.band.setText(empty);
-        } else {
-            holder.band.setText(bandData.get(position).getName());
+            holder.band.setText(bandData2.get(position).getName());
+            holder.bandGenre.setText(bandData2.get(position).getGenre());
         }
 
     }
@@ -61,8 +76,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemCount() {
+        if (flag != true) {
+            return bandData.size();
 
-        return bandData.size();
+        } else {
+            return bandData2.size();
+        }
+
     }
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView band;
--- app/src/main/java/com/metalexplorer/SavedFragment.java
+++ app/src/main/java/com/metalexplorer/SavedFragment.java
@@ -1,19 +1,46 @@
 package com.metalexplorer;
 
+import static com.github.loki.afro.metallum.search.API.getBandById;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.loki.afro.metallum.entity.Band;
+import com.github.loki.afro.metallum.enums.DiscType;
+import com.github.loki.afro.metallum.search.API;
+import com.github.loki.afro.metallum.search.query.entity.BandQuery;
+import com.github.loki.afro.metallum.search.query.entity.DiscQuery;
+import com.github.loki.afro.metallum.search.query.entity.SearchBandResult;
+import com.github.loki.afro.metallum.search.query.entity.SearchDiscResult;
 import com.metalexplorer.databinding.FragmentSavedBinding;
 
-public class SavedFragment extends Fragment {
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SavedFragment extends Fragment implements RecyclerViewInterface {
 
     private FragmentSavedBinding binding;
 
     private View view;
+
+    private RecyclerView recyclerView;
+
+    private RecyclerViewAdapter itemAdapter;
+
+    private ArrayList<Band> itemList = new ArrayList<>();
 
 
 
@@ -21,7 +48,27 @@ public class SavedFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_band_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_saved, container, false);
+
+        List<Long> retrievedLongList = UserBookmarks.getLongList(requireContext(), "user123");
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+
+
+        for (long number : retrievedLongList) {
+            Band band = API.getBandById(number);
+            if (!itemList.contains(band)) {
+                itemList.add(band);
+            }
+        }
+
+        itemAdapter = new RecyclerViewAdapter(itemList, this, recyclerView, true);
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        itemAdapter.notifyDataSetChanged();
         return view;
 
     }
@@ -37,4 +84,77 @@ public class SavedFragment extends Fragment {
         binding = null;
     }
 
+    @Override
+    public void OnItemClick(int position, RecyclerView recyclerView) {
+        BandProfileFragment bandProfileFragment = new BandProfileFragment();
+        Bundle bundle = new Bundle();
+        List<SearchDiscResult> discList1 = new ArrayList<>();
+        int beginDate = itemList.get(position).getYearFormedIn();
+        int currentYear = Year.now().getValue();
+
+        DiscQuery query = DiscQuery.builder()
+                .discType(DiscType.FULL_LENGTH)
+                .bandName(itemList.get(position).getName())
+                .fromYear(beginDate)
+                .toYear(currentYear)
+                .build();
+
+        for (SearchDiscResult discResult : API.getDiscs(query)) {
+            if (discResult.getBandId() == itemList.get(position).getId()) {
+                discList1.add(discResult);
+            }
+        }
+
+        ParcelableDisc parcelableDisc = new ParcelableDisc(discList1);
+
+        bundle.putParcelable("DISCOGRAPHY", parcelableDisc);
+
+        ParcelableLineup parcelableLineup = new ParcelableLineup(itemList.get(position).getCurrentMembers());
+
+        bundle.putParcelable("LINEUP", parcelableLineup);
+
+        ParcelableLink parcelableLinks = new ParcelableLink(itemList.get(position).getLinks());
+
+        bundle.putParcelable("LINKS", parcelableLinks);
+
+        bundle.putString("FORMED", String.valueOf(itemList.get(position).getYearFormedIn()));
+
+        bundle.putString("COUNTRY", itemList.get(position).getCountry().getFullName());
+
+        bundle.putString("GENRE", itemList.get(position).getGenre());
+
+        bundle.putLong("ID", itemList.get(position).getId());
+
+
+
+        Optional<byte[]> optionalPhoto = itemList.get(position).getLogo();
+
+        if (optionalPhoto.isPresent()) {
+            byte[] photoBytes = optionalPhoto.get();
+            bundle.putByteArray("IMAGE", photoBytes);
+
+        } else {
+            bundle.putByteArray("IMAGE", (byte[]) null);
+        }
+
+        Optional<byte[]> optionalPhoto2 = itemList.get(position).getPhoto();
+
+        if (optionalPhoto2.isPresent()) {
+            byte[] photoBytes2 = optionalPhoto2.get();
+            bundle.putByteArray("BAND_PHOTO", photoBytes2);
+
+        } else {
+            bundle.putByteArray("BAND_PHOTO", (byte[]) null);
+        }
+
+        bandProfileFragment.setArguments(bundle);
+
+        FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, bandProfileFragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
 }
